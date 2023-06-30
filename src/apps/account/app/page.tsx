b146fd6c7725fba943f4534f8ca28b5ae491cc82
Wayne Rocha <62760711+WayneRocha@users.noreply.github.com>
2023-06-30 20:56:11 -0300

    remove unused files

--- src/apps/account/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-100 lg:p-4 lg:dark:bg-zinc-800/30">
-        Esse será o subdominio com o dashboard (account.therockets.com)
-      </p>
-    </main>
-  );
-}
