be59010bfe3b7b1d33e97c35ab9d3d843a98104f
Wayne Rocha <62760711+WayneRocha@users.noreply.github.com>
2023-06-30 03:56:17 -0300

    build:project basic setup

---
+++ .storybook/main.js
@@ -0,0 +1,26 @@
+/** @type { import('@storybook/nextjs').StorybookConfig } */
+const config = {
+  stories: [
+    '../apps/ui/**!(/node_modules)/**/*.stories.@(js|jsx|ts|tsx)',
+    '../apps/web/**!(/node_modules)/**/*.stories.@(js|jsx|ts|tsx)',
+    '../apps/account/**!(/node_modules)/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
+  staticDirs: ['../apps/account/public', '../apps/web/public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  core: {
+    builder: '@storybook/builder-webpack5',
+  }
+};
+
+export default config;
