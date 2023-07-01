c1128f4ee96f8cb3bebf10759ea20fba655df8fe
Wayne Rocha <62760711+WayneRocha@users.noreply.github.com>
2023-06-30 23:23:13 -0300

    Update page.tsx

--- src/apps/account/app/page.tsx
+++ src/apps/account/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-100 lg:p-4 lg:dark:bg-zinc-800/30">
-        Esse será o subdominio com o dashboard (account.therockets.com)
+        Esse será o subdominio para administrar plataformas e o perfil do úsuario (account.therockets.com)
       </p>
     </main>
   );
